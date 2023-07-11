ce9a817ee263c162f6ccc302cb8f38fd3e7b7257
consoleprint <137482429+consoleprint@users.noreply.github.com>
2023-07-10 21:43:14 -0700

    add

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -11,8 +11,8 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      Learning TODO List using
+      <a href="https://nodejs.org/" target="_blank" rel="noopener">Nodejs</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
